5d7876137fcb6857d6ef338114c9ae2ad21c3b06
Juan Torres <juanto121@hotmail.com>
2014-10-20 01:51:56 -0500

    basic project structure

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+	app  = express(),
+	server = require('http').createServer(app);
+
+
+server.listen(process.env.PORT || 80);
+
+app.use(express.static( __dirname +'/public'));
+
+app.get('/',function(req,res){
+  res.sendfile( __dirname + '/public/views/kroz.html');
+
+});
---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+var Korz = (function(){
+	function Korz(){
+		this.init();
+	}
+	var korz = Korz.prototype:
+	korz.init() = function(){
+		this.canvas = document.getElementById("canvas");
+	}
+	return Korz;
+})();
+
+window.onload = function(){
+	korz = new Korz();
+}
